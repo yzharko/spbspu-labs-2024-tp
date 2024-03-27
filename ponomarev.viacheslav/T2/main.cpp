3efc8f21d7d038c05e71732f7d41710b21af844e
ponomarev.viacheslav <slava_ponomarev_05@mail.ru>
2024-03-27 03:22:54 +0300

    Added input and output of first key and third key

---
+++ ponomarev.viacheslav/T2/main.cpp
@@ -0,0 +1,201 @@
+// Пример перегрузки ввода/вывода для пользовательского типа Data.
+// Похожим образом можно организовать ввод/вывод в работе 1,
+// но в этом примере имеется ряд упрощений:
+// 1) не поддерживается произвольный порядок полей
+// 2) не поддерживаются строки некорректного формата
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cassert>
+#include <iterator>
+#include <vector>
+#include <iomanip>
+
+namespace ponomarev
+{
+  // формат ввода:
+  // (:key1 5.45e-2:key2 0b1000101:key3 "Data":)
+  struct Data
+  {
+    double key1;
+    unsigned long long key2;
+    std::string key3;
+  };
+
+  struct DelimiterIO
+  {
+    char exp;
+  };
+
+  struct DoubleIO
+  {
+    double &ref;
+  };
+
+  struct LongLongIO
+  {
+    unsigned long long &ref;
+  };
+
+  struct StringIO
+  {
+    std::string &ref;
+  };
+
+  struct LabelIO
+  {
+    std::string exp;
+  };
+
+  // scope guard для возврата состояния потока в первоначальное состояние
+  class iofmtguard
+  {
+  public:
+    iofmtguard(std::basic_ios< char > &s);
+    ~iofmtguard();
+  private:
+    std::basic_ios< char > &s_;
+    char fill_;
+    std::streamsize precision_;
+    std::basic_ios< char >::fmtflags fmt_;
+  };
+
+  std::istream &operator>>(std::istream &in, DelimiterIO &&dest);
+  std::istream &operator>>(std::istream &in, DoubleIO &&dest);
+  std::istream &operator>>(std::istream &in, StringIO &&dest);
+  std::istream &operator>>(std::istream &in, LabelIO &&dest);
+  std::istream &operator>>(std::istream &in, Data &dest);
+  std::ostream &operator<<(std::ostream &out, const Data &dest);
+}
+
+int main()
+{
+  using ponomarev::Data;
+  std::vector< Data > data;
+  std::copy(
+    std::istream_iterator< Data >(std::cin),
+    std::istream_iterator< Data >(),
+    std::back_inserter(data)
+  );
+
+  std::cout << "Data:\n";
+  std::copy(
+    std::begin(data),
+    std::end(data),
+    std::ostream_iterator< Data >(std::cout, "\n")
+  );
+
+  return 0;
+}
+
+namespace ponomarev
+{
+  std::istream &operator>>(std::istream &in, DelimiterIO &&dest)
+  {
+    // все перегрузки операторов ввода/вывода должны начинаться с проверки экземпляра класса sentry
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return in;
+    }
+    char c = '0';
+    in >> c;
+    if (c != dest.exp)
+    {
+      in.setstate(std::ios::failbit);
+    }
+    return in;
+  }
+
+  std::istream &operator>>(std::istream &in, DoubleIO &&dest)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return in;
+    }
+    return in >> dest.ref;
+  }
+
+  std::istream &operator>>(std::istream &in, StringIO &&dest)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return in;
+    }
+    return std::getline(in >> DelimiterIO{ '"' }, dest.ref, '"');
+  }
+
+  std::istream &operator>>(std::istream &in, LabelIO &&dest)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return in;
+    }
+    std::string data;
+    std::getline(in >> DelimiterIO{ ':' }, data, ' ');
+    if (data != dest.exp)
+    {
+      in.setstate(std::ios::failbit);
+    }
+    return in;
+  }
+
+  std::istream &operator>>(std::istream &in, Data &dest)
+  {
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return in;
+    }
+    Data input;
+    {
+      using sep = DelimiterIO;
+      using label = LabelIO;
+      using dbl = DoubleIO;
+      using str = StringIO;
+      in >> sep{ '(' };
+      in >> label{ "key1" } >> dbl{ input.key1 };
+      in >> label{ "key3" } >> str{ input.key3 };
+      in >> sep{ ':' };
+      in >> sep{ ')' };
+    }
+    if (in)
+    {
+      dest = input;
+    }
+    return in;
+  }
+
+  std::ostream &operator<<(std::ostream &out, const Data &src)
+  {
+    std::ostream::sentry sentry(out);
+    if (!sentry)
+    {
+      return out;
+    }
+    iofmtguard fmtguard(out);
+    out << "(:";
+    out << "key1 " << std::setprecision(10) << std::scientific << src.key1 << ":";
+    out << "key3 " << '"' << src.key3 << '"';
+    out << ":)";
+    return out;
+  }
+
+  iofmtguard::iofmtguard(std::basic_ios< char > &s) :
+    s_(s),
+    fill_(s.fill()),
+    precision_(s.precision()),
+    fmt_(s.flags())
+  {}
+
+  iofmtguard::~iofmtguard()
+  {
+    s_.fill(fill_);
+    s_.precision(precision_);
+    s_.flags(fmt_);
+  }
+}
