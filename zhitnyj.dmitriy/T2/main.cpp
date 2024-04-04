334954818058c8569d5008ea3e1b60e83a0f1973
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2024-04-04 18:34:16 +0300

    Create main.cpp

---
+++ zhitnyj.dmitriy/T2/main.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include "DataStruct.h"
+
+int main()
+{
+  std::vector<DataStruct> dataVector;
+
+  std::copy(std::istream_iterator<DataStruct>(std::cin), std::istream_iterator<DataStruct>(), std::back_inserter(dataVector));
+
+  std::sort(dataVector.begin(), dataVector.end(), [](const DataStruct& a, const DataStruct& b)
+  {
+    if (a.key1 != b.key1) return a.key1 < b.key1;
+    if (a.key2 != b.key2) return a.key2 < b.key2;
+    return a.key3.length() < b.key3.length();
+  });
+
+  std::copy(dataVector.begin(), dataVector.end(), std::ostream_iterator<DataStruct>(std::cout, "\n"));
+
+  return 0;
+}
