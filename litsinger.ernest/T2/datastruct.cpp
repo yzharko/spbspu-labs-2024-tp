b9e07c8827ab94e82d8064438b46105871562f69
0412erik <litsinger00@bk.ru>
2024-04-04 14:58:05 +0300

    fix cg 1.3

--- litsinger.ernest/T2/datastruct.cpp
+++ litsinger.ernest/T2/datastruct.cpp
@@ -46,7 +46,8 @@ std::istream& litsinger::operator>>(std::istream& is, litsinger::DataStruct& val
 std::ostream& litsinger::operator<<(std::ostream& os, const litsinger::DataStruct& myStruct)
 {
   iofmtguard fguard(os);
-  os << "(:key1 0b" << std::bitset<8> {myStruct.key1} << ":key2 0x" << std::hex << std::uppercase << myStruct.key2 << ":key3 \"" << myStruct.key3 << "\":)";
+  os << "(:key1 0b" << std::bitset<8> {myStruct.key1} << ":key2 0x" << std::hex
+      << std::uppercase << myStruct.key2 << ":key3 \"" << myStruct.key3 << "\":)";
   return os;
 }
 
