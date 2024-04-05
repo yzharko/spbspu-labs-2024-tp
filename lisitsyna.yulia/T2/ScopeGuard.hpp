e3bf3778082181bb075eca9ef09556107e483e98
unknown <iulia.lis1tsyna@yandex.ru>
2024-04-05 13:15:28 +0300

    fix Scope Gurard

--- lisitsyna.yulia/T2/ScopeGuard.hpp
+++ lisitsyna.yulia/T2/ScopeGuard.hpp
@@ -7,11 +7,23 @@ namespace lisitsyna
   class StreamGuard
   {
     public:
-      StreamGuard(std::basic_ios<char>& s);
-      ~StreamGuard();
+      StreamGuard(std::basic_ios<char>& s):
+        s_(s),
+        fill_(s.fill()),
+        precision_(s.precision()),
+        flags(s.flags())
+      {}
+      ~StreamGuard()
+      {
+        s_.fill(fill_);
+        s_.precision(precision_);
+        s_.flags(flags);
+      }
     private:
+      char fill_
       std::basic_ios<char>& s_;
       std::streamsize precision_;
-      std::basic_ios<char>::fmtflags flags_;
+      std::basic_ios<char>::fmtflags flags;
   };
 }
+#endif
