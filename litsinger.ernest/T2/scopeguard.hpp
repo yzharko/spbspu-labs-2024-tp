fc159221532f2265ab6a82c40e336301100fb121
0412erik <litsinger00@bk.ru>
2024-04-05 17:40:50 +0300

    fix

--- litsinger.ernest/T2/scopeguard.hpp
+++ litsinger.ernest/T2/scopeguard.hpp
@@ -7,7 +7,7 @@ namespace litsinger
   class iofmtguard
   {
   public:
-    iofmtguard(std::basic_ios< char >& s);
+    explicit iofmtguard(std::basic_ios< char >& s);
     ~iofmtguard();
   private:
     std::basic_ios< char >& s_;
