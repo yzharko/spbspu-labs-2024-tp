7974d31e2e48a56245a7e657e70715cf6b1f432d
unknown <iulia.lis1tsyna@yandex.ru>
2024-04-05 22:04:59 +0300

    adding pattern.cpp

--- lisitsyna.yulia/T2/pattern.cpp
+++ lisitsyna.yulia/T2/pattern.cpp
@@ -2,32 +2,32 @@
 #include "delimiter.hpp"
 #include "ScopeGuard.hpp"
 
-std::istream & lisitsyna::operator>>(std::istream & in, SllLit && exp)
+std::istream& lisitsyna::operator>>(std::istream& in, SllLit&& exp)
 {
-  std::istream::sentry guard(in);
-  if (!guard)
-  {
-    return in;
-  }
-  return in >> exp.ref >> Separator{ 'l' } >> Separator{ 'l' };
+    std::istream::sentry guard(in);
+    if (!guard)
+    {
+        return in;
+    }
+    return in >> exp.ref >> Separator{ 'l' } >> Separator{ 'l' };
 }
 std::istream& lisitsyna::operator>>(std::istream& in, ChrLit&& exp)
 {
-  std::istream::sentry guard(in);
-  if (!guard)
-  {
+    std::istream::sentry guard(in);
+    if (!guard)
+    {
+        return in;
+    }
+    in >> Separator{ '\'' } >> exp.ref >> Separator{ '\'' };
     return in;
-  }
-  in >> Separator{'\''} >> exp.ref >> Separator{'\''};
-  return in;
 }
-std::istream& kozlov::operator>>(std::istream& in, StringVal&& key)
+std::istream& lisitsyna::operator>>(std::istream& in, String&& exp)
 {
-  std::istream::sentry guard(in);
-  if (!guard)
-  {
+    std::istream::sentry guard(in);
+    if (!guard)
+    {
+        return in;
+    }
+    std::getline(in >> Separator{ '"' }, exp.ref, '"');
     return in;
-  }
-  std::getline(in >> Separator{'"'}, exp.ref, '"');
-  return in;
 }
