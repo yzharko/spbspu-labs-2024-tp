3fb0feda2eec97fca917d16ba401373f10bc15fa
phohlova <pv.hohlova@edu.spbstu.ru>
2024-04-05 20:06:24 +0300

    -

---
+++ hohlova.polina/T2/main.cpp
@@ -0,0 +1,31 @@
+#include <limits>
+#include <algorithm>
+#include "structures.hpp"
+
+int main()
+{
+  using namespace hohlova;
+  std::vector<Data> data;
+
+  while (!std::cin.eof())
+  {
+    std::copy(
+      std::istream_iterator<Data>{std::cin},
+      std::istream_iterator<Data>{},
+      std::back_inserter(data));
+    if (std::cin.fail())
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+  }
+
+  std::sort(data.begin(), data.end());
+
+  std::copy(
+    std::begin(data),
+    std::end(data),
+    std::ostream_iterator<Data>(std::cout, "\n"));
+
+  return 0;
+}
