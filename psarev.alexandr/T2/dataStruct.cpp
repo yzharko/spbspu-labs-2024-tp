dbd8a981cc05e5c6dbc8b930fc1aafb40dce3691
Александр Псарев <alexandrpsarev5555@gmail.com>
2024-04-07 14:37:23 +0300

    "<" overload fixed

--- psarev.alexandr/T2/dataStruct.cpp
+++ psarev.alexandr/T2/dataStruct.cpp
@@ -58,7 +58,7 @@ bool psarev::operator<(const DataStruct& first, const DataStruct& second)
 {
   if (first.key1 != second.key1)
   {
-    return first.key1 < second.key2;
+    return first.key1 < second.key1;
   }
   else if (first.key2 != second.key2)
   {
