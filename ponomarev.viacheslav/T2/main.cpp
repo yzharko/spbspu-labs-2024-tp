0186e1a36005d00dc0cd1a45fd7a0d91c86895d5
ponomarev.viacheslav <slava_ponomarev_05@mail.ru>
2024-04-10 15:00:22 +0300

    Added check form first key

--- ponomarev.viacheslav/T2/main.cpp
+++ ponomarev.viacheslav/T2/main.cpp
@@ -6,6 +6,7 @@
 #include <iterator>
 #include <vector>
 #include <limits>
+#include <cmath>
 
 namespace ponomarev
 {
@@ -134,7 +135,67 @@ namespace ponomarev
     {
       return in;
     }
-    return in >> dest.ref;
+    std::string numberStr;
+    std::string degreeStr;
+    char symbol = '0';
+    char sign;
+    const char temp = 'e';
+    in >> symbol;
+    numberStr += symbol;
+    numberStr += '.';
+    in >> symbol;
+    if (symbol != '.')
+    {
+      in.setstate(std::ios::failbit);
+      return in;
+    }
+    in >> symbol;
+
+    while (std::isdigit(symbol))
+    {
+      numberStr += symbol;
+      in >> symbol;
+    }
+
+    symbol = tolower(symbol);
+    if (symbol == temp)
+    {
+      in >> sign;
+    }
+    else
+    {
+      in.setstate(std::ios::failbit);
+      return in;
+    }
+
+    in >> symbol;
+
+    while (std::isdigit(symbol))
+    {
+      degreeStr += symbol;
+      in >> symbol;
+    }
+
+    if (symbol != ':')
+    {
+      in.setstate(std::ios::failbit);
+      return in;
+    }
+    double numberDoub = std::stod(numberStr);
+    double degreeInt = std::stoi(degreeStr);
+    if (sign == '+')
+    {
+      dest.ref = numberDoub * std::pow(10, degreeInt);
+    }
+    else if (sign == '-')
+    {
+      dest.ref = numberDoub * std::pow(10, -degreeInt);
+    }
+    else
+    {
+      in.setstate(std::ios::failbit);
+    }
+    return in;
   }
 
   std::istream &operator>>(std::istream &in, StringIO &&dest)
@@ -160,17 +221,17 @@ namespace ponomarev
       using dbl = DoubleIO;
       using lolo = LongLongIO;
       using str = StringIO;
-      std::string typeOfKey;
+      std::string typeOfKey = "key3";
       in >> sep{ '(' };
       for (size_t i = 0; i < 3; i++)
       {
-        if (typeOfKey == "key2")
+        if (typeOfKey == "key3")
         {
-          std::getline(in , typeOfKey, ' ');
+          std::getline(in >> DelimiterIO{ ':' }, typeOfKey, ' ');
         }
         else
         {
-          std::getline(in >> DelimiterIO{ ':' }, typeOfKey, ' ');
+          std::getline(in , typeOfKey, ' ');
         }
 
         if (typeOfKey == "key1")
@@ -221,7 +282,7 @@ namespace ponomarev
     double mantis = src.key1;
     int degree = 0;
     out << "(:";
-    if (mantis < 10)
+    if (0 < mantis < 10)
     {
       while (mantis < 1)
       {
