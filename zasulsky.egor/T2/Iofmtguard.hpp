a4c9c98e71b6ac9e953cd2d33acac80f3ceec01c
leopepsi <3756035@gmail.com>
2024-04-12 13:47:28 +0300

    passed lab

---
+++ zasulsky.egor/T2/Iofmtguard.hpp
@@ -0,0 +1,23 @@
+ifndef IOFMTGUARD_HPP
+#define IOFMTGUARD_HPP
+
+#include <iomanip>
+
+namespace zasulsky
+{
+  class iofmtguard
+  {
+  public:
+    iofmtguard(std::basic_ios< char >& s);
+    ~iofmtguard();
+
+  private:
+    std::basic_ios< char >& s_;
+    char fill_;
+    std::streamsize precision_;
+    std::basic_ios< char >::fmtflags fmt_;
+  };
+}
+
+#endif
+
