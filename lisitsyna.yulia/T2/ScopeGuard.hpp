63f50df4607428cf915092a1aa637e4800f135c6
unknown <iulia.lis1tsyna@yandex.ru>
2024-04-15 14:53:41 +0300

    fix Scopeguard

--- lisitsyna.yulia/T2/ScopeGuard.hpp
+++ lisitsyna.yulia/T2/ScopeGuard.hpp
@@ -1,29 +1,19 @@
 #ifndef STREAM_GUARD_HPP
 #define STREAM_GUARD_HPP
-#include <iostream>
+#include <ios>
 
 namespace lisitsyna
 {
   class StreamGuard
   {
-    public:
-      StreamGuard(std::basic_ios<char>& s):
-        s_(s),
-        fill_(s.fill()),
-        precision_(s.precision()),
-        flags_(s.flags())
-      {}
-      ~StreamGuard()
-      {
-        s_.fill(fill_);
-        s_.precision(precision_);
-        s_.flags(flags_);
-      }
-    private:
-      char fill_;
-      std::basic_ios<char>& s_;
-      std::streamsize precision_;
-      std::basic_ios<char>::fmtflags flags_;
+  public:
+    explicit StreamGuard(std::basic_ios<char>& s);
+    ~StreamGuard()
+  private:
+    std::basic_ios<char>& s_;
+    std::streamsize precision_;
+    char fill_;
+    std::basic_ios<char>::fmtflags fmt_;
   };
 }
 #endif
