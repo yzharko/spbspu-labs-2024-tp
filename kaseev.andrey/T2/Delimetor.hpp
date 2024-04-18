35405a059bf2c3ba264af081fcc5cd4a20138157
herznaetkcto <147743642+herznaetkcto@users.noreply.github.com>
2024-04-18 04:26:49 +0300

    delimetor

---
+++ kaseev.andrey/T2/Delimetor.hpp
@@ -0,0 +1,14 @@
+#ifndef DELIMETOR_HPP
+#define DELIMETOR_HPP
+
+#include <iostream>
+
+namespace kaseev {
+  struct Delimiter {
+    char expected;
+  };
+
+  std::istream &operator>>(std::istream &is, Delimiter &&exp);
+}
+
+#endif
