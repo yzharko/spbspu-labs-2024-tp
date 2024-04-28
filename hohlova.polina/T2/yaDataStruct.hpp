5c747defc6695802d62dd89426973a1bb76fca46
phohlova <pv.hohlova@edu.spbstu.ru>
2024-04-28 21:33:27 +0300

    yaDataStruct

---
+++ hohlova.polina/T2/yaDataStruct.hpp
@@ -0,0 +1,25 @@
+#ifndef YADATASTRUCT_HPP
+#define YADATASTURCT_HPP
+#include <complex>
+#include <cassert>
+#include <iomanip>
+
+namespace hohlova
+{
+  using real = std::pair<long long, unsigned long long>;
+  using sll = long long;
+  using ull = unsigned long long;
+
+  struct Data
+  {
+    real key1;
+    std::complex< double > key2;
+    std::string key3;
+  };
+  std::istream& operator>>(std::istream& in, Data& dest);
+  std::ostream& operator<<(std::ostream& out, const Data& src);
+
+  bool operator<(const Data& lhs, const Data& rhs);
+};
+
+#endif
