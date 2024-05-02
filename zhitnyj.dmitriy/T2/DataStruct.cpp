f664301e5446b6cc2a0f441e7649075384a329e6
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2024-05-02 18:32:14 +0300

    Update DataStruct.cpp

--- zhitnyj.dmitriy/T2/DataStruct.cpp
+++ zhitnyj.dmitriy/T2/DataStruct.cpp
@@ -3,8 +3,14 @@
 
 bool DataStruct::operator<(const DataStruct& other) const
 {
-  if (key1 != other.key1) return key1 < other.key1;
-  if (key2 != other.key2) return key2 < other.key2;
+  if (key1 != other.key1)
+  {
+    return key1 < other.key1;
+  }
+  if (key2 != other.key2)
+  {
+    return key2 < other.key2;
+  }
   return key3.length() < other.key3.length();
 }
 
