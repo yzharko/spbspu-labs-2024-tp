e6eeebf1dea29708250f36f64a5e457fd3af41c2
urtanto <anikanovvlad@yandex.ru>
2024-05-05 14:40:28 +0300

    test

--- anikanov.vladislav/T2/mainExtension.hpp
+++ anikanov.vladislav/T2/mainExtension.hpp
@@ -4,7 +4,7 @@
 #include "dataStruct.hpp"
 
 namespace anikanov {
-  bool dataStructComparator(const DataStruct &a, const DataStruct &b);
+  bool dataStructComparator(const anikanov::DataStruct &a, const anikanov::DataStruct &b);
 }
 
 #endif
