ed168139f21be4c89b2a400d90301f6f87c1f2fc
urtanto <anikanovvlad@yandex.ru>
2024-05-05 14:17:54 +0300

    finish dataStruct.cpp

--- anikanov.vladislav/T2/dataStruct.cpp
+++ anikanov.vladislav/T2/dataStruct.cpp
@@ -15,6 +15,98 @@ std::istream &anikanov::operator>>(std::istream &in, anikanov::DelimiterIO &&des
   return in;
 }
 
+std::istream &anikanov::operator>>(std::istream &in, anikanov::UllLitIO &&dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry) {
+    return in;
+  }
+
+  in >> dest.ref >> anikanov::DelimiterIO{'u'} >> anikanov::DelimiterIO{'l'} >> anikanov::DelimiterIO{'l'};
+  if (!in) {
+    in.setstate(std::ios::failbit);
+  }
+  return in;
+}
+
+std::istream &anikanov::operator>>(std::istream &in, anikanov::UllBinIO &&dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry) {
+    return in;
+  }
+  return in >> std::oct >> dest.ref;
+}
+
+std::istream &anikanov::operator>>(std::istream &in, anikanov::StringIO &&dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry) {
+    return in;
+  }
+  return std::getline(in >> anikanov::DelimiterIO{'"'}, dest.ref, '"');
+}
+
+std::istream &anikanov::operator>>(std::istream &in, anikanov::DelStrIO &&dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry) {
+    return in;
+  }
+  size_t i = 0;
+  while (dest.exp[i] != '\0') {
+    in >> DelimiterIO({dest.exp[i++]});
+  }
+  return in;
+}
+
+std::istream &anikanov::operator>>(std::istream &in, anikanov::DataStruct &dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry) {
+    return in;
+  }
+
+  using del = DelimiterIO;
+  using delStr = DelStrIO;
+  using lit = UllLitIO;
+  using bin = UllBinIO;
+  using str = StringIO;
+
+  DataStruct input;
+
+  in >> delStr({"(:"});
+  int number = 0;
+
+  for (size_t i = 0; i < 3; i++) {
+    in >> delStr({"key"}) >> number;
+
+    switch (number) {
+      case 1:
+        in >> lit{input.key1};
+        break;
+      case 2:
+        in >> std::oct >> bin{input.key2} >> std::dec;
+        break;
+      case 3:
+        in >> str{input.key3};
+        break;
+      default:
+        in.setstate(std::ios::failbit);
+        break;
+    }
+
+    in >> del{':'};
+  }
+
+  in >> del{')'};
+
+  if (in) {
+    dest = input;
+  }
+  return in;
+}
+
 std::ostream &operator<<(std::ostream &out, const anikanov::DataStruct &src)
 {
   std::ostream::sentry sentry(out);
@@ -23,7 +115,7 @@ std::ostream &operator<<(std::ostream &out, const anikanov::DataStruct &src)
   }
   anikanov::iofmtguard fmtguard(out);
   out << "(:key1 " << src.key1 << "ull";
-  out << ":key2 0" << std::oct << src.key2;
+  out << ":key2 0b" << std::oct << src.key2;
   out << ":key3 \"" << src.key3 << "\":)";
   return out;
 }
