a48d64c0ff0b59c70c78f14d815f9539056d9ac9
Arina Redko <arina.redko.05@mail.ru>
2024-05-06 13:47:57 +0300

    fixed declaration

--- redko.arina/T3/shape.hpp
+++ redko.arina/T3/shape.hpp
@@ -10,31 +10,12 @@ namespace redko
     int x, y;
   };
 
-  int diffMultPair(const Point & firstP, const Point & secondP);
-  Point makeVector(const Point & firstP, const Point & secondP);
-  int makeScalar(const Point & firstV, const Point & secondV);
-  int makeVectorMultiply(const Point & point, const Point & firstP, const Point & secondP);
-
   std::istream & operator>>(std::istream & in, Point & dest);
 
   struct Polygon
   {
     std::vector< Point > points;
   };
-
-  double countArea(const Polygon & shape);
-  double sumAreas(double res, const Polygon & shape);
-  double sumAreasIfEven(double res, const Polygon & shape);
-  double sumAreasIfOdd(double res, const Polygon & shape);
-  double sumAreasIfEqual(double res, const Polygon & shape, size_t numOfVertexes);
-  int getNumOfVertexes(const Polygon & shape);
-  bool isNumOfVertexesEven(const Polygon shape);
-  bool isNumOfVertexesOdd(const Polygon shape);
-  bool isNumOfVertexesEqual(const Polygon shape, size_t numOfVertexes);
-  bool isPointInPolygon(const Point & point, const Polygon & shape);
-  bool isIntersected(const Polygon & firstS, const Polygon & secondS);
-  bool isEqual(int firstN, int secondN);
-  bool isRightShape(const Polygon & shape);
 
   std::istream & operator>>(std::istream & in, Polygon & dest);
 }
