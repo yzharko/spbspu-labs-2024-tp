cef57a2f60d7364e035194f0f4e8a5b615d30a6e
ponomarev.viacheslav <slava_ponomarev_05@mail.ru>
2024-05-06 13:47:43 +0300

    Added common and input file in main

---
+++ ponomarev.viacheslav/T3/main.cpp
@@ -0,0 +1,21 @@
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <functional>
+#include <limits>
+
+int main(int argc, char * argv[])
+{
+  if (argc != 2)
+  {
+    std::cerr << "Error: wrong number of parameters\n";
+    return 2;
+  }
+
+  std::ifstream input(argv[1]);
+  if (!input)
+  {
+    std::cerr << "Error: unable to open the file\n";
+    return 2;
+  }
+}
