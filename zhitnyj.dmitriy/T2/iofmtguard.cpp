856db181827e77061d57fa897ad7da294040a168
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2024-05-07 17:06:04 +0300

    Update iofmtguard.cpp

--- zhitnyj.dmitriy/T2/iofmtguard.cpp
+++ zhitnyj.dmitriy/T2/iofmtguard.cpp
@@ -1,8 +1,7 @@
 #include "iofmtguard.hpp"
 
 iofmtguard::iofmtguard(std::basic_ios< char >& s) : s_(s), fill_(s.fill()), precision_(s.precision()), fmt_(s.flags())
-{
-}
+{}
 
 iofmtguard::~iofmtguard()
 {
