8ee30d1eff2aea77b539264b59b6b1bb1b62ac63
jirkov17 <jirkoff17@gmail.com>
2024-05-08 03:31:03 +0300

    перегрузил операторы ввода и вывода для ключа 3

--- jirkov.vladimir/T2/DataStruct.cpp
+++ jirkov.vladimir/T2/DataStruct.cpp
@@ -2,22 +2,74 @@
 
 std::istream & jirkov::operator>>(std::istream &in, DataStruct &value)
 {
-  //some
+  std::istream::sentry guard(in);
+  if(!guard)
+  {
+    return in;
+  }
+  StreamGuard fmtguard(in);
+  std::string key = "";
+  DataStruct input;
+  in >> Del{'('} >> Del{':'};
+  for(int i = 0; i < 3; i++)
+  {
+    in >> key;
+    if(key == "key1")
+    {
+      in >> /*name key1*/{input.key1} >> Del{':'};
+    }
+    else if(key == "key2")
+    {
+      is >> /*name key2*/{input.key2} >> Del{':'};
+    }
+    else if(key == "key3")
+    {
+      is >> Str{input.key3} >> Del{':'};
+    }
+    else
+    {
+      in.setstate(std::ios::failbit);
+    }
+  }
+  in >> Del{')'};
+  if(in)
+  {
+    value = input;
+  }
   return in;
 }
 
 std::ostream & jirkov::operator<<(std::ostream & out, const DataStruct &value)
 {
-  //some2
+  std::ostream::sentry guard(out);
+  if(!guard)
+  {
+    return out;
+  }
+  StreamGuard fmtguard(out);
+  out << "(:key1 " << '\'' << value.key1 << '\'' << ":key2 (:N " << value.key2 << ":):key3 " << "\"" << value.key3 << "\":)";
   return out;
 }
 
 bool jirkov::operator>(const DataStruct &left, const DataStruct &right)
 {
- //some3
-  return false;
+  if(left.key1 != right.key1)
+  {
+    //return
+  }
+  else if (left.key2 != right.key2)
+  {
+    //return
+  }
+  else if (left.key3.length() != right.key3.length())
+  {
+    return left.key3.length() > right.key3.length();
+  }
+  else
+  {
+    return false;
+  }
 }
-
 bool jirkov::operator<(const DataStruct &left, const DataStruct &right)
 {
   return !(left > right);
