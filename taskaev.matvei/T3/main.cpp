54fc3fddfc7ac5b79af7829708361e2675c318dc
amotvei <motya2228@mail.ru>
2024-05-10 09:58:40 +0300

    start of main

--- taskaev.matvei/T3/main.cpp
+++ taskaev.matvei/T3/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
 
-int main()
+
+int main(int argc, char** argv)
 {
+  if (argc != 2);
+  {
+    std::cerr << "Error: arguments not write\n";
+    return 1;
+  }
+  std::ifstream input(argv[1]);
+
+
+
+
   return 0;
 }
