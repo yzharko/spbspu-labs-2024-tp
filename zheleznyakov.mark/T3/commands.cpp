33aa5c49261c9bb73331cbd3a38a3d92ca2934be
Mark Zheleznyakov <mrqiz@internet.ru>
2024-05-10 21:17:17 +0300

    feat(T3): AREA с EVEN, ODD, MEAN, n-vertexes

--- zheleznyakov.mark/T3/commands.cpp
+++ zheleznyakov.mark/T3/commands.cpp
@@ -1,9 +1,67 @@
 #include "commands.hpp"
+#include <functional>
+#include <limits>
+#include <iostream>
+#include <numeric>
 
 std::ostream & zheleznyakov::commands::area(const std::vector< Polygon > & polygons, std::istream & in, std::ostream & out)
 {
-  std::string r;
-  in >> r;
-  out << "Got argument: " << r << "\n";
-  return out << polygons.size();
+  std::string subCmd = "";
+  in >> subCmd;
+  out << std::fixed << std::setprecision(1);
+  if (subCmd == "EVEN")
+  {
+    return out << processAreaEven(polygons) << '\n';
+  }
+  else if (subCmd == "ODD")
+  {
+    return out << processAreaOdd(polygons) << '\n';
+  }
+  else if (subCmd == "MEAN")
+  {
+    return out << processAreaMean(polygons) << '\n';
+  }
+  size_t vertexes = std::stoll(subCmd);
+  return out << processAreaVertexes(polygons, vertexes) << '\n';
+}
+
+double zheleznyakov::areaAccumulator(double currentSum, const Polygon & poly)
+{
+  return currentSum + calculatePolygonArea(poly, 0, 0.0);
+}
+
+double zheleznyakov::oddAreaAccumulator(double currentSum, const Polygon & poly)
+{
+  return currentSum + (poly.points.size() % 2 != 0 ? calculatePolygonArea(poly, 0, 0.0) : 0);
+}
+
+double zheleznyakov::evenAreaAccumulator(double currentSum, const Polygon & poly)
+{
+  return currentSum + (poly.points.size() % 2 == 0 ? calculatePolygonArea(poly, 0, 0.0) : 0);
+}
+
+double zheleznyakov::vertexAreaAccumulator(double currentSum, const Polygon & poly, size_t vertexes)
+{
+  return currentSum + (poly.points.size() == vertexes ? calculatePolygonArea(poly, 0, 0.0) : 0);
+}
+
+double zheleznyakov::processAreaEven(const std::vector< Polygon > & polys)
+{
+  return std::accumulate(polys.begin(), polys.end(), 0, evenAreaAccumulator);
+}
+
+double zheleznyakov::processAreaOdd(const std::vector< Polygon > & polys)
+{
+  return std::accumulate(polys.begin(), polys.end(), 0, oddAreaAccumulator);
+}
+
+double zheleznyakov::processAreaMean(const std::vector< Polygon > & polys)
+{
+  return std::accumulate(polys.begin(), polys.end(), 0, areaAccumulator) / polys.size();
+}
+
+double zheleznyakov::processAreaVertexes(const std::vector< Polygon > & polys, size_t vertexes)
+{
+  using namespace std::placeholders;
+  return std::accumulate(polys.begin(), polys.end(), 0, std::bind(vertexAreaAccumulator, _1, _2, vertexes));
 }
--- zheleznyakov.mark/T3/commands.hpp
+++ zheleznyakov.mark/T3/commands.hpp
@@ -6,7 +6,17 @@ namespace zheleznyakov
 {
   namespace commands
   {
-    std::ostream & area(const std::vector < Polygon > &, std::istream &, std::ostream &);
+    std::ostream & area(const std::vector< Polygon > &, std::istream &, std::ostream &);
   }
+
+  double areaAccumulator(double currentSum, const Polygon & poly);
+  double oddAreaAccumulator(double currentSum, const Polygon & poly);
+  double evenAreaAccumulator(double currentSum, const Polygon & poly);
+  double vertexAreaAccumulator(double currentSum, const Polygon & poly, size_t vertexes);
+
+  double processAreaEven(const std::vector< Polygon > & polys);
+  double processAreaOdd(const std::vector< Polygon > & polys);
+  double processAreaMean(const std::vector< Polygon > & polys);
+  double processAreaVertexes(const std::vector< Polygon > & polys, size_t vertexes);
 }
 #endif
