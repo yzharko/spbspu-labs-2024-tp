dd7e1c2a1694158903d111ff0e021546b6168e8c
amotvei <motya2228@mail.ru>
2024-05-10 14:54:54 +0300

    fixed in base variant

--- taskaev.matvei/T3/main.cpp
+++ taskaev.matvei/T3/main.cpp
@@ -34,6 +34,9 @@ int main(int argc, char** argv)
     cmds["MAX"] = std::bind(MaxComand, _1, _2, _3);
     cmds["MIN"] = std::bind(MinComand, _1, _2, _3);
     cmds["COUNT"] = std::bind(CountComand, _1, _2, _3);
+    // later
+    // MaxSeq
+    // Same
   }
 
   std::string cmd = "";
