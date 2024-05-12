d6e162b51341632a804480b77994dcfbe800b8d5
anyadg <anna_giraffe@mail.ru>
2024-05-12 19:10:07 +0300

    added another exception

--- doroshenko.anna/T3/main.cpp
+++ doroshenko.anna/T3/main.cpp
@@ -48,8 +48,14 @@ int main(int argc, char* argv[])
     catch(const std::invalid_argument& e)
     {
       std::cout << e.what() << "\n";
+      std::cin.clear();
       std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
     }
+    catch(const std::out_of_range& e)
+    {
+      std::cout << "<INVALID COMMAND>\n";
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
   }
   return 0;
 }
