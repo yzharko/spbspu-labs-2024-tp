50e5f01a7897c1476b27bf304534b40cc3dbb501
anyadg <anna_giraffe@mail.ru>
2024-05-12 19:13:11 +0300

    build error

--- doroshenko.anna/T3/main.cpp
+++ doroshenko.anna/T3/main.cpp
@@ -56,6 +56,7 @@ int main(int argc, char* argv[])
       std::cout << "<INVALID COMMAND>\n";
       std::cin.clear();
       std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
   }
   return 0;
 }
