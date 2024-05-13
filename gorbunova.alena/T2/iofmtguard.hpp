b5148374ea0582def396b201dc65157ab28ed31f
allleng05 <allleng04@mail.ru>
2024-05-13 16:38:10 +0300

    fix

--- gorbunova.alena/T2/iofmtguard.hpp
+++ gorbunova.alena/T2/iofmtguard.hpp
@@ -7,13 +7,13 @@ namespace gorbunova
   class iofmtguard
   {
   public:
-      explicit iofmtguard(std::basic_ios< char > &s);
-      ~iofmtguard();
+    explicit iofmtguard(std::basic_ios< char > &s);
+    ~iofmtguard();
   private:
-      std::basic_ios< char > &s_;
-      char fill_;
-      std::streamsize precision_;
-      std::basic_ios< char >::fmtflags fmt_;
+    std::basic_ios< char > &s_;
+    char fill_;
+    std::streamsize precision_;
+    std::basic_ios< char >::fmtflags fmt_;
   };
 }
 #endif
