3e19eb3d53573777f927ffd4a405a7518811413a
Anikanov Vlad <anikanovvlad@yandex.ru>
2024-05-14 11:40:56 +0300

    Polygon created # Without CG

---
+++ anikanov.vladislav/T3/polygon.hpp
@@ -0,0 +1,13 @@
+#ifndef POLYGON_HPP
+#define POLYGON_HPP
+
+#include <vector>
+#include "point.hpp"
+
+namespace anikanov {
+    struct Polygon {
+        std::vector<Point> points;
+    };
+}
+
+#endif
