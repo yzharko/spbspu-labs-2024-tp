6536dc31cac53bef5df30453b494879e57d5ba01
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2024-05-14 04:42:56 +0300

    Update Commands.hpp

--- zhitnyj.dmitriy/T3/Commands.hpp
+++ zhitnyj.dmitriy/T3/Commands.hpp
@@ -8,28 +8,28 @@
 class Commands
 {
 public:
-    explicit Commands(std::vector< Polygon >& polygons);
+  explicit Commands(std::vector< Polygon >& polygons);
 
-    void areaCommand(std::istream& input, std::ostream& output);
-    void maxCommand(std::istream& input, std::ostream& output);
-    void minCommand(std::istream& input, std::ostream& output);
-    void countCommand(std::istream& input, std::ostream& output);
-    void rmechoCommand(std::istream& input, std::ostream& output);
-    void permsCommand(std::istream& input, std::ostream& output);
+  void areaCommand(std::istream& input, std::ostream& output);
+  void maxCommand(std::istream& input, std::ostream& output);
+  void minCommand(std::istream& input, std::ostream& output);
+  void countCommand(std::istream& input, std::ostream& output);
+  void rmechoCommand(std::istream& input, std::ostream& output);
+  void permsCommand(std::istream& input, std::ostream& output);
 
 private:
-    std::vector< Polygon >& polygons;
+  std::vector< Polygon >& polygons;
 
-    double calculateAreaEvenOdd(bool even) const;
-    double calculateMeanArea() const;
-    double calculateAreaByVertexCount(int count) const;
-    double calculateMaxArea() const;
-    int calculateMaxVertexes() const;
-    double calculateMinArea() const;
-    int calculateMinVertexes() const;
-    int countPolygons(bool even, int vertex_count = -1) const;
-    int countPerms(const Polygon& target) const;
-    int removeEcho(const Polygon& target);
+  double calculateAreaEvenOdd(bool even) const;
+  double calculateMeanArea() const;
+  double calculateAreaByVertexCount(int count) const;
+  double calculateMaxArea() const;
+  int calculateMaxVertexes() const;
+  double calculateMinArea() const;
+  int calculateMinVertexes() const;
+  int countPolygons(bool even, int vertex_count = -1) const;
+  int countPerms(const Polygon& target) const;
+  int removeEcho(const Polygon& target);
 };
 
 #endif
