ea9fa4847661de590799b6ed494838b4d927b1d5
Anikanov Vlad <anikanovvlad@yandex.ru>
2024-05-14 12:23:53 +0300

    New point # Without CG

---
+++ anikanov.vladislav/T3/point.cpp
@@ -0,0 +1,19 @@
+#include "point.hpp"
+
+#include <cmath>
+#include <iostream>
+#include "../common/inOutPut.hpp"
+
+anikanov::Point &anikanov::Point::operator=(const anikanov::Point &point) {
+    x = point.x;
+    y = point.y;
+    return *this;
+}
+
+double anikanov::Point::operator-(const anikanov::Point &point) const {
+    return sqrt(pow(x - point.x, 2) + pow(y - point.y, 2));
+}
+
+std::istream &anikanov::operator>>(std::istream &in, anikanov::Point &dest) {
+    return in >> DelimiterIO{'('} >> dest.x >> DelimiterIO{';'} >> dest.y >> DelimiterIO{')'};
+}
---
+++ anikanov.vladislav/T3/point.hpp
@@ -0,0 +1,20 @@
+#ifndef POINT_H
+#define POINT_H
+
+#include <iosfwd>
+
+namespace anikanov {
+    struct Point {
+        int x;
+        int y;
+
+        Point() : x(0), y(0) {}
+        Point(int x, int y): x(x), y(y) {}
+        Point &operator=(const Point &point);
+        double operator-(const Point &point) const;
+    };
+
+    std::istream &operator >>(std::istream & in, Point& dest);
+}
+
+#endif
