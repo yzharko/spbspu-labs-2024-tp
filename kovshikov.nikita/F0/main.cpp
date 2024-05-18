dda820ebc2ea503b5e70bc94f1eae1166d139db7
harduserNIKITA <cumpotttt@yandex.ru>
2024-05-18 12:36:54 +0300

    for BUILD

--- kovshikov.nikita/F0/main.cpp
+++ kovshikov.nikita/F0/main.cpp
@@ -32,35 +32,4 @@ int main()
       std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
     }
   }
-
-
-
-/*  try
-  {
-    using namespace kovshikov;
-    Graph graph;
-    graph.addVertex(1, "никита");
-    graph.addVertex(2, "сессия");
-    std::cout << graph.isEmpty() << "\n";
-    std::cout << graph.getSize() << "\n";
-    graph.outKeys();
-    graph.deleteVertex(1);
-    graph.outKeys();
-    graph.addVertex(3, "вова");
-    graph.createEdge(2, 3, 100);
-    graph.decreaseWeight(2, 3, 1);
-    std::cout << graph.getWeight(2, 3) << "\n";
-    graph.deleteEdge(2, 3);
-    std::cout << graph.getWeight(2, 3) << "\n";
-   // graph.deleteVertex(1);
-    graph.createEdge(2, 3, 10);
-    std::cout << graph.getWeight(2, 3) << "\n";
-    std::cout << graph.getWeight(1, 3) << "\n";
-    std::cout << graph.getWeight(2, 4) << "\n";
-    std::cout << graph.getWeight(1, 4) << "\n";
-  }
-  catch(const std::exception& error)
-  {
-    std::cout << error.what() << "\n";
-  }*/
 }
