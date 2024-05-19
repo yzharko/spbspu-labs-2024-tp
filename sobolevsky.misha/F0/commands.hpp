09490c67723c744cf9fc8a37ec00f561f4c8aa0d
han7ter <sobmisha12@gmail.com>
2024-05-19 21:33:59 +0300

    че вообще проиходит, как так

--- sobolevsky.misha/F0/commands.hpp
+++ sobolevsky.misha/F0/commands.hpp
@@ -10,25 +10,30 @@
 namespace sobolevsky
 {
   void getCommands(std::istream & in, std::ostream & out);
-  void getLoadAndCreate(std::shared_ptr< std::vector< std::pair< std::string, std::multimap< size_t, std::string > > > > myVec,
+  void getLoadAndCreate(std::shared_ptr< std::vector< std::pair< std::string, std::multimap< size_t,
+  std::string > > > > myVec, std::istream & in, std::ostream & out);
+  void getDelete(std::shared_ptr< std::vector< std::pair< std::string, std::multimap< size_t,
+  std::string > > > > myVec, std::istream & in, std::ostream & out);
+  void getAllDicts(std::shared_ptr< std::vector< std::pair< std::string, std::multimap< size_t,
+  std::string > > > > myVec, std::istream & in, std::ostream & out);
+  void getSelect(std::shared_ptr< std::vector< std::pair< std::string, std::multimap< size_t,
+  std::string > > > > myVec, std::istream & in, std::ostream & out);
+  void holyTrinity(std::pair< std::string, std::multimap< size_t, std::string > > &myPair,
   std::istream & in, std::ostream & out);
-  void getDelete(std::shared_ptr< std::vector< std::pair< std::string, std::multimap< size_t, std::string > > > > myVec,
+  void printDict(std::pair< std::string, std::multimap< size_t, std::string > > &myPair,
   std::istream & in, std::ostream & out);
-  void getAllDicts(std::shared_ptr< std::vector< std::pair< std::string, std::multimap< size_t, std::string > > > > myVec,
+  void uniqeWords(std::pair< std::string, std::multimap< size_t, std::string > > &myPair,
   std::istream & in, std::ostream & out);
-  void getSelect(std::shared_ptr< std::vector< std::pair< std::string, std::multimap< size_t, std::string > > > > myVec,
+  void wordCount(std::pair< std::string, std::multimap< size_t, std::string > > &myPair,
   std::istream & in, std::ostream & out);
-  void holyTrinity(std::pair< std::string, std::multimap< size_t, std::string > > &myPair, std::istream & in, std::ostream & out);
-  void printDict(std::pair< std::string, std::multimap< size_t, std::string > > &myPair, std::istream & in, std::ostream & out);
-  void uniqeWords(std::pair< std::string, std::multimap< size_t, std::string > > &myPair, std::istream & in, std::ostream & out);
-  void wordCount(std::pair< std::string, std::multimap< size_t, std::string > > &myPair, std::istream & in, std::ostream & out);
-  void getRename(std::shared_ptr< std::vector< std::pair< std::string, std::multimap< size_t, std::string > > > > myVec,
-  std::istream & in, std::ostream & out);
+  void getRename(std::shared_ptr< std::vector< std::pair< std::string, std::multimap< size_t,
+  std::string > > > > myVec, std::istream & in, std::ostream & out);
   void save(std::pair< std::string, std::multimap< size_t, std::string > > &myPair, std::istream & in);
 
   void error(std::ostream & out, const std::string &text);
   bool isNameHere(std::pair< std::string, std::multimap< size_t, std::string > > &pair, const std::string &name);
-  void dictOutput(std::pair< std::string, std::multimap< size_t, std::string > > &myPair, std::ostream & out, size_t n, bool mode);
+  void dictOutput(std::pair< std::string, std::multimap< size_t, std::string > > &myPair,
+  std::ostream & out, size_t n, bool mode);
 }
 
 #endif
