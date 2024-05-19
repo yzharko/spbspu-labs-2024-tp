34dedfed21c6c1a26f8a81d0fe289ee7d027efaf
oksidmargantsa <sukachva.maria@gmail.com>
2024-05-20 02:52:47 +0300

    fixing content

--- sukacheva.maria/F0/workspace.hpp
+++ sukacheva.maria/F0/workspace.hpp
@@ -6,27 +6,27 @@ namespace sukacheva
 {
   struct Workspace
   {
-	Workspace(Graph actualGraph_): actualGraph(actualGraph_), activityFlag(true) {}
-	~Workspace() = default;
-	Graph actualGraph;
-	bool activityFlag;
+    Workspace(Graph actualGraph_): actualGraph(actualGraph_), activityFlag(true) {}
+    ~Workspace() = default;
+    Graph actualGraph;
+    bool activityFlag;
   };
 
   struct GraphList
   {
-	GraphList() = default;
-	~GraphList() = default;
-	std::map< std::string, Workspace > graphList;
-	Graph findActiveWorkspace()
-	{
-	  for (auto it = graphList.begin(); it != graphList.end(); ++it)
-	  {
-		if (it->second.activityFlag)
-		{
-		  return it->second.actualGraph;
-		}
-	  }
-	}
+    GraphList() = default;
+    ~GraphList() = default;
+    std::map< std::string, Workspace > graphList;
+    Graph findActiveWorkspace()
+    {
+      for (auto it = graphList.begin(); it != graphList.end(); ++it)
+      {
+        if (it->second.activityFlag)
+        {
+          return it->second.actualGraph;
+        }
+      }
+    }
   };
 }
 
