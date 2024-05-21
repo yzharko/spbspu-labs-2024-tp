3a78ceec4ceb031b6312e7ca8d75b021acf9230f
Александр Псарев <alexandrpsarev5555@gmail.com>
2024-05-21 22:41:56 +0300

    internaluncs started

---
+++ psarev.alexandr/F0/internalFuncs.hpp
@@ -0,0 +1,20 @@
+#ifndef INTERNAL_FUNCS_HPP
+#define INTERNAL_FUNCS_HPP
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+using storage_t = std::map< std::string, std::vector< std::string > >;
+
+namespace psarev
+{
+  std::ostream& outError(std::ostream& out, const std::string& errText);
+  storage_t readStorage(std::istream& in);
+  void cleanUpWord(std::string& word);
+  bool checkLiter(const std::string& word);
+  std::string getSpType(std::string& word);
+  bool checkType(std::string& word, size_t& endSize, std::vector< std::string >& ends);
+}
+
+#endif
