15437b5c3e891e4ee01825e20cab66005986f1c7
zhitnyjda <148652180+zhitnyjda@users.noreply.github.com>
2024-05-29 03:59:00 +0300

    Update GraphUtils.hpp

--- zhitnyj.dmitriy/F0/GraphUtils.hpp
+++ zhitnyj.dmitriy/F0/GraphUtils.hpp
@@ -1,7 +1,7 @@
 #ifndef GRAPHUTILS_HPP
 #define GRAPHUTILS_HPP
 
-#include <iostream>
+#include <iosfwd>
 #include <map>
 #include "Graph.hpp"
 
@@ -9,9 +9,8 @@ void addVertex(Graph &graph, const std::string &vertex);
 void addEdge(Graph &graph, const std::string &vertex1, const std::string &vertex2, int weight);
 void deleteVertex(Graph &graph, const std::string &vertex);
 void deleteEdge(Graph &graph, const std::string &vertex1, const std::string &vertex2);
-void showGraph(const Graph &graph);
 void updateEdge(Graph &graph, const std::string &vertex1, const std::string &vertex2, int newWeight);
-void neighbors(const Graph &graph, const std::string &vertex);
-void isConnected(const Graph &graph, const std::string &vertex1, const std::string &vertex2);
+void neighbors(const Graph &graph, const std::string &vertex, std::ostream &output);
+void isConnected(const Graph &graph, const std::string &vertex1, const std::string &vertex2, std::ostream &output);
 
 #endif
