0612ccbd21a7ad5b6cf328d4afd9d3da3ac7c2d2
tsomossi <battsolmon.a20sur11a@gmail.com>
2024-09-16 15:30:12 +0300

    chika

--- battsolmon.ankhbayar/T2/main.cpp
+++ battsolmon.ankhbayar/T2/main.cpp
@@ -5,36 +5,36 @@
 
 using namespace ankhbayar;
 
-int main(void) //(:key1 01001:key2 0xFFFA:key3 "":)
+int main(void)  // (:key1 01001:key2 0xFFFA:key3 "Data":)
 {
   std::vector<DataStruct> vec;
   while (!std::cin.eof())
   {
     std::cin.clear();
     std::copy(
-      std::istream_iterator<DataStruct>(std::cin),
-      std::istream_iterator<DataStruct>(),
-      std::back_inserter(vec)
+    std::istream_iterator<DataStruct>(std::cin),
+    std::istream_iterator<DataStruct>(),
+    std::back_inserter(vec)
     );
   }
   std::sort(
   vec.begin(),
   vec.end(),
-  [](const DataStruct& lhs, const DataStruct& rhs)
+   [](const DataStruct& lhs, const DataStruct& rhs)
    {
-     if(lhs.key1<rhs.key1)
+     if (lhs.key1 < rhs.key1)
      {
        return true;
      }
-     if(lhs.key2<rhs.key2)
+     if (lhs.key2 < rhs.key2)
      {
-       return true;
+        return true;
      }
      else
      {
-       return lhs.key3 < rhs.key3;
+        return lhs.key3 < rhs.key3;
      }
    }
-  );
-  std::copy(vec.begin(), vec.end(), std::ostream_iterator<DataStruct>(std::cout, "\n"));
+ );
+ std::copy(vec.begin(), vec.end(), std::ostream_iterator<DataStruct>(std::cout, "\n"));
 }
