f118ccf7989ca1f7df8d57d42cfed6625636b219
zeneco <nmalanin05@gmail.com>
2024-09-17 23:53:14 +0300

    labT2

---
+++ malanin.nikita/T2/main.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <iterator>
+#include <vector>
+#include <limits>
+#include "dataStruct.hpp"
+
+int main()
+{
+  using malanin::yaDataStruct;
+  std::vector< yaDataStruct > data;
+  while (!std::cin.eof())
+  {
+    std::copy(
+      std::istream_iterator< yaDataStruct >(std::cin),
+      std::istream_iterator< yaDataStruct >(),
+      std::back_inserter(data)
+    );
+    if (std::cin.fail() && !std::cin.eof())
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+    }
+  }
+  std::sort(data.begin(), data.end());
+  std::copy(
+    std::begin(data),
+    std::end(data),
+    std::ostream_iterator< yaDataStruct >(std::cout, "\n")
+  );
+  return 0;
+}
