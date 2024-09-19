40ac2288ae65d26c038a4d5d96cb31a7aeafce4e
Oğulcan Enes Uzun <ogulcannenes@Ogulcans-MacBook-Pro.local>
2024-09-19 20:33:31 +0300

    T2

---
+++ uzun.oguldzhan/T2/DelimiterIO.hpp
@@ -0,0 +1,12 @@
+#ifndef DELIMITER_IO_HPP
+#define DELIMITER_IO_HPP
+
+namespace uzun
+{
+  struct DelimiterIO
+  {
+    char exp;
+  };
+}
+
+#endif
---
+++ uzun.oguldzhan/T2/UllOctIO.hpp
@@ -0,0 +1,12 @@
+#ifndef ULLLITIO_HPP
+#define ULLLITIO_HPP
+
+namespace uzun
+{
+  struct UllOctIO
+  {
+    unsigned long long &ref;
+  };
+}
+
+#endif
