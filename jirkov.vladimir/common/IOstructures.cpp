5d611986681da3a7541ab3165faf66c3cc90c583
jirkov17 <jirkoff17@gmail.com>
2024-09-19 20:26:30 +0300

    add

--- jirkov.vladimir/common/IOstructures.cpp
+++ jirkov.vladimir/common/IOstructures.cpp
@@ -1,6 +1,10 @@
 #include "IOstructures.hpp"
-#include "DelimiterIO.hpp"
 #include <iostream>
+#include "DelimiterIO.hpp"
+#include "UllOctIO.hpp"
+#include "UllLitIO.hpp"
+#include "StringIO.hpp"
+#include "DataStruct.hpp"
 std::istream & jirkov::operator>>(std::istream & in, DelimiterIO && value)
 {
   std::istream::sentry guard(in);
@@ -15,4 +19,36 @@ std::istream & jirkov::operator>>(std::istream & in, DelimiterIO && value)
     in.setstate(std::ios::failbit);
   }
   return in;
+}
+
+std::istream & jirkov::operator>>(std::istream & in, UllLitIO && value)
+{
+  std::istream::sentry guard(in);
+  if (!guard)
+  {
+    return in;
+  }
+  return in >> value.ref >> DelimiterIO{'u'} >> DelimiterIO{'l'} >> DelimiterIO{'l'};
+}
+
+std::istream & jirkov::operator>>(std::istream & in, UllOctIO && value)
+{
+  std::istream::sentry guard(in);
+  if (!guard)
+  {
+    return in;
+  }
+  using del = DelimiterIO;
+  in >> del{'0'};
+  return in >> std::oct >> value.ref;
+}
+
+std::istream & jirkov::operator>>(std::istream & in, StringIO && value)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry)
+  {
+    return in;
+  }
+  return std::getline(in >> DelimiterIO{ '"' }, value.str, '"');
 }
