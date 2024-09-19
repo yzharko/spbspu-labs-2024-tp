0cce3f06b8bd6193554340474646318eb1688425
ilyapanov123 <g89517673116@gmail.com>
2024-09-19 17:11:31 +0300

    ds

--- panov.ilya/T2/main.cpp
+++ panov.ilya/T2/main.cpp
@@ -4,29 +4,29 @@
 #include <iterator>
 #include "DataStruct.h"
 
-namespace panov
+int main()
 {
-  int main()
-  {
-    std::vector<DataStruct> dataVec;
+  using namespace panov;
+  std::vector<DataStruct> dataVec;
 
-    std::copy(std::istream_iterator<DataStruct>(std::cin),
-      std::istream_iterator<DataStruct>(),
-      std::back_inserter(dataVec));
+  std::copy(std::istream_iterator<DataStruct>(std::cin),
+    std::istream_iterator<DataStruct>(),
+    std::back_inserter(dataVec));
 
-    std::sort(dataVec.begin(), dataVec.end(), [](const DataStruct& lhs, const DataStruct& rhs)
+  std::sort(dataVec.begin(), dataVec.end(), [](const DataStruct& lhs, const DataStruct& rhs)
+    {
+      if (lhs.key1 != rhs.key1)
       {
-        if (lhs.key1 != rhs.key1)
-        {
-          return std::abs(lhs.key1) < std::abs(rhs.key1);
-        }
-        if (lhs.key2 != rhs.key2)
-        {
-          return lhs.key2 < rhs.key2;
-        }
-        return lhs.key3.size() < rhs.key3.size();
-      });
-    std::copy(dataVec.begin(), dataVec.end(), std::ostream_iterator<DataStruct>(std::cout, "\n"));
-    return 0;
-  }
+        return std::abs(lhs.key1) < std::abs(rhs.key1);
+      }
+      if (lhs.key2 != rhs.key2)
+      {
+        return lhs.key2 < rhs.key2;
+      }
+      return lhs.key3.size() < rhs.key3.size();
+    });
+
+  std::copy(dataVec.begin(), dataVec.end(), std::ostream_iterator<DataStruct>(std::cout, "\n"));
+
+  return 0;
 }
