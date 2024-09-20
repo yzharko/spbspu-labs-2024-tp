601f00ff008c87e8350a3012fccd83ee84c0c6f2
GuyFawkes00 <spproselkov@mail.ru>
2024-09-20 19:22:07 +0300

    fix

--- proselkov.stanislav/F0/commands.hpp
+++ proselkov.stanislav/F0/commands.hpp
@@ -6,21 +6,21 @@
 
 namespace proselkov
 {
-  using langDictionary = std::map< std::string, std::string >;
-  using majorDictionary = std::map< std::string, langDictionary >;
+	using langDictionary = std::map< std::string, std::string >;
+	using majorDictionary = std::map< std::string, langDictionary >;
 
-  void helpCmd(std::istream& in, std::ostream& out);
-  void createCmd(majorDictionary& major, std::istream& in, std::ostream& out);
-  void removeCmd(majorDictionary& major, std::istream& in, std::ostream& out);
-  void printCmd(majorDictionary& major, std::istream& in, std::ostream& out);
-  void clearCmd(majorDictionary& major, std::istream& in, std::ostream& out);
-  void deleteCmd(majorDictionary& major, std::istream& in, std::ostream& out);
-  void insertCmd(majorDictionary& major, std::istream& in, std::ostream& out);
-  void searchCmd(majorDictionary& major, std::istream& in, std::ostream& out);
-  void sizeCmd(majorDictionary& major, std::istream& in, std::ostream& out);
-  void editCmd(majorDictionary& major, std::istream& in, std::ostream& out);
-  void combineCmd(majorDictionary& major, std::istream& in, std::ostream& out);
-  void saveCmd(majorDictionary& major, std::istream& in, std::ostream& out);
+	void helpCmd(std::istream& in, std::ostream& out);
+	void createCmd(majorDictionary& major, std::istream& in, std::ostream& out);
+	void removeCmd(majorDictionary& major, std::istream& in, std::ostream& out);
+	void printCmd(majorDictionary& major, std::istream& in, std::ostream& out);
+	void clearCmd(majorDictionary& major, std::istream& in, std::ostream& out);
+	void deleteCmd(majorDictionary& major, std::istream& in, std::ostream& out);
+	void insertCmd(majorDictionary& major, std::istream& in, std::ostream& out);
+	void searchCmd(majorDictionary& major, std::istream& in, std::ostream& out);
+	void sizeCmd(majorDictionary& major, std::istream& in, std::ostream& out);
+	void editCmd(majorDictionary& major, std::istream& in, std::ostream& out);
+	void combineCmd(majorDictionary& major, std::istream& in, std::ostream& out);
+	void saveCmd(majorDictionary& major, std::istream& in, std::ostream& out);
 }
 
 #endif
