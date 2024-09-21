747e3f5d1a3cb3615764b70a42a323b8dc282f03
tsomossi <battsolmon.a20sur11a@gmail.com>
2024-09-21 17:26:03 +0300

    err

--- battsolmon.ankhbayar/F0/Main.cpp
+++ battsolmon.ankhbayar/F0/Main.cpp
@@ -10,15 +10,15 @@ using namespace std;
 
 int main() {
   try {
-      Graph g;
+    Graph g;
 
-      std::string cmd;
-      while (std::getline(std::cin, cmd)) {
-        bool res = process(cmd, g);
-        if (!res) {
-             invalidCommandMessage();
-        }
+    std::string cmd;
+    while (std::getline(std::cin, cmd)) {
+      bool res = process(cmd, g);
+      if (!res) {
+         invalidCommandMessage();
       }
+    }
   }
   catch (const std::invalid_argument& ex) {
     std::cerr << ex.what();
