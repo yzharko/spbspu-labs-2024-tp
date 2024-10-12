fec25a3b87d9df9fe976fea10369ded0f6ca09f6
ankhbayar battsolmon <battsolmon.a20sur11a@gmail.com>
2024-10-12 17:36:21 +0300

    iii

--- battsolmon.ankhbayar/T3/main.cpp
+++ battsolmon.ankhbayar/T3/main.cpp
@@ -1,68 +1,52 @@
-#include <algorithm>
-#include <climits>
-#include <fstream>
-#include <functional>
-#include <iomanip>
 #include <iostream>
-#include <map>
-#include <stdexcept>
-#include "commands.hpp"
-#include "figures.hpp"
+#include <fstream>
+#include <limits>
+#include "Functions.h"
+#include "io.h"
 
-using namespace std;
-
-int main(int argc, char* argv[])
+int main(int argc, char** argv)
 {
-    if (argc != 2)
-    {
-        std::cerr << "Error: Expected 1 command-line argument, but got " << argc - 1 << ".\n";
-        return EXIT_FAILURE;
-    }
-    std::string fileName = argv[1];
-    std::ifstream file(fileName);
-    if (!file)
-    {
-        std::cerr << "Error: file didn't open\n";
-        return EXIT_FAILURE;
-    }
+  using namespace ankh;
 
-    std::cout << std::setprecision(1) << std::fixed;
-    std::vector< std::Polygon > polygons;
-    while (!file.eof())
+  if (argc != 2) {
+    std::cerr << "BAD PARAMETERS" << '\n';
+    return -1;
+  }
+  std::string file = argv[1];
+  std::ifstream input(file);
+  Polygon polygon;
+  std::vector<Polygon> polygons;
+
+  if (!input.is_open())
+  {
+    std::cerr << "BAD FILE" << '\n';
+    return -1;
+  }
+
+  while (!input.eof())
+  {
+    input >> polygon;
+    if (!input.fail())
     {
-        if (!file.eof() && file.fail())
-        {
-            file.clear();
-            file.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
-        }
+      polygons.push_back(polygon);
     }
-    std::map< std::string, std::function< void(std::istream&, std::ostream&) > > commands;
+    else
     {
-        using namespace std::placeholders;
+      input.clear();
+      input.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
-    std::string cmd;
-    while (std::cin >> cmd)
+  }
+
+  while (!std::cin.eof())
+  {
+    try
     {
-        if (std::cin.eof())
-        {
-            break;
-        }
-        try
-        {
-            commands.at(cmd)(std::cin, std::cout);
-        }
-        catch (const std::out_of_range&)
-        {
-            std::cout << "<INVALID COMMAND>\n";
-            std::cin.clear();
-            std::cin.ignore(INT_MAX, '\n');
-        }
-        catch (const std::invalid_argument& e)
-        {
-            std::cout << e.what() << '\n';
-            std::cin.clear();
-            std::cin.ignore(INT_MAX, '\n');
-        }
+      chooseCommand(std::cin, std::cout, polygons);
     }
-    return 0;
+    catch (std::exception& e)
+    {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+  }
 }
